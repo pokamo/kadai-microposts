ab8d9278413a6c4829118641e15e8136f1684aa4
pokamo <ppokamo@gmail.com>
2021-11-09 23:34:16 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,8 +17,16 @@ class UsersController extends Controller
     public function show($id) {
         $user = User::findOrFail($id);
         
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザの投稿一覧を作成日時の降順で取得
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+
+        // ユーザ詳細ビューでそれらを表示
         return view('users.show', [
             'user' => $user,
-            ]);
+            'microposts' => $microposts,
+        ]);
     }
 }
